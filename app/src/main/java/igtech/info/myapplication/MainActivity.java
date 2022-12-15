d3331450f26d69e976921208295c8f6b4e0ef2e6
Rabidalton <gibsonjohn626@gmail.com>
2022-12-15 22:18:49 +0000

    This is my second project on calculator

---
+++ app/src/main/java/igtech/info/myapplication/MainActivity.java
@@ -0,0 +1,9 @@
+package igtech.info.myapplication;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void onDigit(View view) {
+    }
+}
